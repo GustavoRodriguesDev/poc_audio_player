38403f5de448eb438460a215f2863d4878bdb759
Gustavo Rodrigues <gustavorpsantos1999@gmail.com>
2023-03-22 15:29:12 -0300

    init_repo

---
+++ android/app/src/main/kotlin/com/example/pock_audio_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pock_audio_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/player_audio_controller.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class PlayerAudioController {
+  final player = AudioPlayer();
+
+  final playerEnumNotifier = ValueNotifier<PlayerEnum>(PlayerEnum.loading);
+
+  Duration totalDurationAudio = Duration.zero;
+  final currentDurationAudioNotifier = ValueNotifier<Duration>(Duration.zero);
+  Future<void> setUrlAudio(String urlAudio) async {
+    final durationAudio = await player.setUrl(urlAudio);
+    if (durationAudio != null) {
+      totalDurationAudio = durationAudio;
+    }
+    _initializeAudio();
+  }
+
+  Future<void> setLocalPathAudio(String pathAudio) async {
+    final durationAudio = await player.setUrl(pathAudio);
+    if (durationAudio != null) {
+      totalDurationAudio = durationAudio;
+    }
+    _initializeAudio();
+  }
+
+  void _initializeAudio() {
+    player.positionStream.listen((currenteAudio) {
+      currentDurationAudioNotifier.value = currenteAudio;
+    });
+
+    player.playerStateStream.listen((state) {
+      if (!state.playing) {
+        playerEnumNotifier.value = PlayerEnum.pause;
+      } else {
+        switch (state.processingState) {
+          case ProcessingState.loading:
+            playerEnumNotifier.value = PlayerEnum.loading;
+            break;
+          case ProcessingState.buffering:
+            playerEnumNotifier.value = PlayerEnum.loading;
+            break;
+          case ProcessingState.idle:
+            playerEnumNotifier.value = PlayerEnum.loading;
+            break;
+          case ProcessingState.ready:
+            playerEnumNotifier.value = PlayerEnum.play;
+            player.play();
+            break;
+          case ProcessingState.completed:
+            player.seek(Duration.zero);
+            player.pause();
+            break;
+        }
+      }
+    });
+  }
+
+  Future<void> play() async {
+    await player.play();
+  }
+
+  Future<void> pause() async {
+    await player.pause();
+  }
+
+  Future<void> add() async {
+    await player
+        .seek(currentDurationAudioNotifier.value + const Duration(seconds: 10));
+  }
+
+  Future<void> subtract() async {
+    await player
+        .seek(currentDurationAudioNotifier.value - const Duration(seconds: 10));
+  }
+
+  void dispose() {
+    player.dispose();
+  }
+}
+
+enum PlayerEnum {
+  loading,
+  play,
+  pause,
+}
